84f3679ad5be27f6660b304c02a1bfdeaa4a78a9
ap <arsenicpaper9@gmail.com>
2024-01-25 17:59:57 +0300

    second commit

--- GameEngine/Classes.py
+++ GameEngine/Classes.py
@@ -1,36 +1,36 @@
-import pygame
+import pygame, random
 
 
 class MainMenu:
     def __init__(self, screen, screen_res):
         self.title_background = pygame.image.load("Assets/Sprites/TitleScreen/Background/title_screen_background.png")
         self.title_background = pygame.transform.scale(self.title_background, screen_res)
+        pygame.mixer.music.load("Assets/Sounds/menu_theme.mp3")
+        pygame.mixer.music.play(-1)
         self.screen = screen
         self.screen_res = screen_res
         self.title_cups = list()
         self.cup_res = (int(self.screen_res[0] * 0.67578125), int(self.screen_res[1] * 0.694))
 
         for i in range(1, 35):  # loading filenames of CUPHEAD and MUGMAN images into title_cups
-            if i < 10:
-                title_cup = pygame.image.load(f"Assets/Sprites/TitleScreen/Cups/cuphead_title_screen_000{i}.png")
-            else:
-                title_cup = pygame.image.load(f"Assets/Sprites/TitleScreen/Cups/cuphead_title_screen_00{i}.png")
-
+            title_cup = pygame.image.load(f"Assets/Sprites/TitleScreen/Cups/cuphead_title_screen_{i:04}.png")
             title_cup = pygame.transform.scale(title_cup, self.cup_res)
             self.title_cups.append(title_cup)
 
         self.current_cup_index = 0  # current index of cup image
         self.cup_display_timer = 0  # timer to control how long each cup will be displayed
-        self.cup_display_duration = 5 # display duration
+        self.cup_display_duration = 2  # display duration
 
-    def update(self):
+    def update_title(self):
         self.cup_display_timer += 1
         if self.cup_display_timer >= self.cup_display_duration:
             self.cup_display_timer = 0
             self.current_cup_index = (self.current_cup_index + 1) % len(self.title_cups)
 
-    def draw(self):
+    def draw_title(self):
         self.screen.blit(self.title_background, (0, 0))
         current_cup = self.title_cups[self.current_cup_index]
         self.screen.blit(current_cup, (((self.screen_res[0] - self.cup_res[0]) // 2), self.screen_res[1] - self.cup_res[1]))
+        self.update_title()
         pygame.display.update()
+
--- main.py
+++ main.py
@@ -8,18 +8,18 @@ screen_height = 720
 screen_res = (screen_width, screen_height)
 
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("CupHead")
+pygame.display.set_caption("Cuphead")
 
 
 main_menu = MainMenu(screen, screen_res)
 clock = pygame.time.Clock()
 
 running = True
-clock.tick(60)
+
 while running:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    main_menu.update()
-    main_menu.draw()
+    main_menu.draw_title()
 pygame.quit()
